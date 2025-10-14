c230f340ce6b7dde061faa79031b52fdc0ab3dde
Layek Miah <layekofficial63@gmail.com>
2025-10-14 21:15:07 +0600

    start working on backend

--- app/_components/CabinCard.js
+++ app/_components/CabinCard.js
@@ -1,5 +1,6 @@
 import { UsersIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
+import Link from "next/link";
 
 function CabinCard({ cabin }) {
   const { id, name, maxCapacity, regularPrice, discount, image } = cabin;
@@ -10,6 +11,7 @@ function CabinCard({ cabin }) {
         src={image}
         alt={`Cabin ${name}`}
         className="flex-1 border-r border-primary-800"
+        width={300}
       />
 
       <div className="flex-grow">
@@ -43,12 +45,12 @@ function CabinCard({ cabin }) {
         </div>
 
         <div className="bg-primary-950 border-t border-t-primary-800 text-right">
-          <a
+          <Link
             href={`/cabins/${id}`}
             className="border-l border-primary-800 py-4 px-6 inline-block hover:bg-accent-600 transition-all hover:text-primary-900"
           >
             Details & reservation &rarr;
-          </a>
+          </Link>
         </div>
       </div>
     </div>
--- app/layout.js
+++ app/layout.js
@@ -20,9 +20,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen flex flex-col`}>
+      <body
+        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen flex flex-col`}
+      >
         <Header />
-        
+
         <div className="flex-1 px-8 py-12">
           <main className="max-w-7xl mx-auto w-full">{children}</main>
         </div>
