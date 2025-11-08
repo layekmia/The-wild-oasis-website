59304aa7caf1198b8db38b1ade77893b3893b169
Layek Miah <layekofficial63@gmail.com>
2025-11-08 19:12:10 +0600

    testing purpose for production

--- src/components/CabinList.tsx
+++ src/components/CabinList.tsx
@@ -4,7 +4,15 @@ import { ICabin } from "@/types/models";
 
 export default async function CabinList() {
   // const { data } = await getCabins();
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/cabins`);
+  const res = await fetch(
+    `${
+      process.env.NEXT_PUBLIC_BASE_URL
+        ? process.env.NEXT_PUBLIC_BASE_URL + "/api/cabins"
+        : "http://localhost:3000/api/cabins"
+    }`,
+    { cache: "no-store" }
+  );
+
   const cabins: ICabin[] = await res.json();
   return (
     <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
