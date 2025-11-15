14e82678214db9c4ffa83b86bd7289c76ac0de7f
Layek Miah <layekofficial63@gmail.com>
2025-11-15 22:39:33 +0600

    modify sesson providers

--- src/helpers/getSession.ts
+++ src/helpers/getSession.ts
@@ -1,6 +1,6 @@
-// import { getServerSession } from "next-auth";
-// import { authOptions } from "@/app/api/auth/[...nextauth]/option";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/option";
 
-// export async function getSession() {
-//   return await getServerSession(authOptions);
-// }
+export async function getSession() {
+  return await getServerSession(authOptions);
+}
